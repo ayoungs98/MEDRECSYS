18f67a7bb18bc41048860e3889a0e33abe8c391f
Andrew Youngs <97192413+ayoungs98@users.noreply.github.com>
2024-03-25 05:41:18 -0700

    Base React code
    Base React code

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+const App = () => {
+  return (
+    <div>This is the app</div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
